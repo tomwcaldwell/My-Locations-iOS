15ad6b5e1e9ecd34341cc9d4bf784a9492818694
WIND Site <thomascaldwell1@hotmail.co.uk>
2017-06-07 12:44:31 +0100

    Initial commit

---
+++ MapViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MapViewController.swift
+//  Memorable Places
+//
+//  Created by Thomas W Caldwell on 07/06/2017.
+//  Copyright © 2017 Tom Caldwell. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print("Map loaded!")
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
