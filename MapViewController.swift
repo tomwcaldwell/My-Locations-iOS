95696d7a7a6eed48c6e8c401aa3247aee1ddb909
WIND Site <thomascaldwell1@hotmail.co.uk>
2017-06-07 14:55:32 +0100

    Completion commit

--- MapViewController.swift
+++ MapViewController.swift
@@ -7,15 +7,155 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet var map: MKMapView!
+    
+    var manager = CLLocationManager()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Map loaded!")
+        let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(MapViewController.longPress(gestureRecognizer:)))
+        
+        uilpgr.minimumPressDuration = 2
+        
+        map.addGestureRecognizer(uilpgr)
+        
+        
+        if activePlace == -1 {
+            
+            manager.delegate = self
+            manager.desiredAccuracy = kCLLocationAccuracyBest
+            manager.requestWhenInUseAuthorization()
+            manager.startUpdatingLocation()
+            
+        } else {
+            
+            // Get place details to display on map
+            
+            if places.count > activePlace {
+                
+                if let name = places[activePlace]["name"] {
+                    
+                    if let lat = places[activePlace]["lat"] {
+                        
+                        if let lon = places[activePlace]["lon"] {
+                            
+                            if let latitude = Double(lat) {
+                                
+                                if let longitude = Double(lon) {
+                                    
+                                    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                                    
+                                    let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                                    
+                                    let region = MKCoordinateRegion(center: coordinate, span: span)
+                                    
+                                    self.map.setRegion(region, animated: true)
+                                    
+                                    let annotation = MKPointAnnotation()
+                                    
+                                    annotation.coordinate = coordinate
+                                    
+                                    annotation.title = name
+                                    
+                                    self.map.addAnnotation(annotation)
+                                    
+                                }
+                                
+                            }
+                            
+                        }
+                        
+                    }
+                    
+                }
+                
+            }
+            
+        }
 
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func longPress(gestureRecognizer: UILongPressGestureRecognizer) {
+        
+        if gestureRecognizer.state == UIGestureRecognizerState.began {
+        
+            let touchPoint = gestureRecognizer.location(in: self.map)
+            
+            let newCoordinate = self.map.convert(touchPoint, toCoordinateFrom: self.map)
+            
+            let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+            
+            
+            var title = ""
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (placemarks, error) in
+                
+                if error != nil {
+                    
+                    print(error)
+                    
+                } else {
+                    
+                    if let placemark = placemarks?[0] {
+                        
+                        if placemark.subThoroughfare != nil {
+                            
+                            title += placemark.subThoroughfare! + " "
+                            
+                        }
+                        
+                        if placemark.thoroughfare != nil {
+                            
+                            title += placemark.thoroughfare!
+                            
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+                if title == "" {
+                    
+                    title = "Added \(NSDate())"
+                    
+                }
+                
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = newCoordinate
+                
+                annotation.title = title
+                
+                self.map.addAnnotation(annotation)
+                
+                places.append(["name":title, "lat":String(newCoordinate.latitude), "lon":String(newCoordinate.longitude)])
+                
+                UserDefaults.standard.set(places, forKey: "places")
+                
+            })
+
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.09, longitudeDelta: 0.09)
+        let region = MKCoordinateRegion(center: location, span: span)
+        self.map.setRegion(region, animated: true)
+        self.map.showsUserLocation = true
+        
     }
 
     override func didReceiveMemoryWarning() {
--- TableViewController.swift
+++ TableViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
+var places = [Dictionary<String, String>()]
+var activePlace = -1
+
 class TableViewController: UITableViewController {
 
+    @IBOutlet var table: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print("Table loaded!")
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if let tempPlaces = UserDefaults.standard.object(forKey: "places") as? [Dictionary<String, String>] {
+            
+            places = tempPlaces
+        }
+        
+        if places.count == 1 && places[0].count == 0 {
+            
+            places.remove(at: 0)
+            
+            places.append(["name":"Taj Mahal", "lat":"27.176277", "lon":"78.042128"])
+            
+            UserDefaults.standard.set(places, forKey: "places")
+            
+        }
+        
+        activePlace = -1
+        table.reloadData()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,24 +52,56 @@ class TableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return places.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
-        // Configure the cell...
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+        
+        if places[indexPath.row]["name"] != nil {
+            
+            cell.textLabel?.text = places[indexPath.row]["name"]
+            
+        }
+        
+        
 
         return cell
     }
-    */
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        activePlace = indexPath.row
+        
+        performSegue(withIdentifier: "toMap", sender: nil)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.delete {
+            
+            places.remove(at: indexPath.row)
+            
+            UserDefaults.standard.set(places, forKey: "places")
+            
+            table.reloadData()
+            
+        }
+        
+    }
+    
 
     /*
     // Override to support conditional editing of the table view.
